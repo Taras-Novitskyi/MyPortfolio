c4e9bdd179b0fb828e3b90d920cd027fedc40ac1
Taras-Novitskyi <novitskyitaras@gmail.com>
2023-07-19 23:12:18 +0300

    add hero title, photo

--- src/components/Hero/Hero.styled.ts
+++ src/components/Hero/Hero.styled.ts
@@ -15,7 +15,57 @@ export const Bg = styled.div`
 
 export const Content = styled.div`
   display: flex;
-  gap: 30px;
+  flex-direction: column-reverse;
+  gap: 40px;
+
+  margin-top: 100px;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+
+  @media screen and (min-width: 1440px) {
+    /* display: flex; */
+  }
 `;
 
-export const Photo = styled.div``;
+export const Introduction = styled.div`
+  /* text-align: center; */
+
+  @media screen and (min-width: 768px) {
+    text-align: right;
+  }
+`;
+
+export const Suptitle = styled.span`
+  font-family: "Kaushan Script", cursive;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 2;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 50px;
+
+  font-family: "Montserrat", sans-serif;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 1.4;
+
+  @media screen and (min-width: 768px) {
+    font-size: 28px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    font-size: 36px;
+  }
+`;
+
+export const Photo = styled.div`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  /* overflow: hidden; */
+
+  box-shadow: 4px 4px 24px 4px rgba(66, 68, 90, 1);
+`;
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,22 +3,31 @@ import React from "react";
 import avatar from "../../images/IMG_3207.jpg";
 import { Container } from "../Container/Container";
 import { Button } from "../Button/Button";
-import { Bg, Content, Photo } from "./Hero.styled";
+import {
+  Bg,
+  Content,
+  Photo,
+  Introduction,
+  Title,
+  Suptitle,
+} from "./Hero.styled";
 
 export const Hero: React.FC = () => {
   return (
     <Bg>
       <Container>
         <Content>
-          <div>
-            <h1>
-              Hi there I'm Taras. I build and develop sites for digital
-              companies.
-            </h1>
+          <Introduction>
+            <Suptitle>Hi there, I'm Taras.</Suptitle>
+            <Title>
+              Skilled and innovative IT developer dedicated to delivering
+              efficient software solutions, leveraging cutting-edge technologies
+              and fostering collaboration for success.
+            </Title>
             <Button type="button">Request a project</Button>
-          </div>
+          </Introduction>
           <Photo>
-            <img src={avatar} alt="Taras" sizes="580px" />
+            <img src={avatar} alt="Taras" width={343} height={343} />
           </Photo>
         </Content>
       </Container>
